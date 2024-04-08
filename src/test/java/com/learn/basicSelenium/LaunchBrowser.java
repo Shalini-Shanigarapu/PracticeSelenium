c66258fd246651c8397b3fc9a8bc3bd9363625ed
Shalini Shanigarapu <shalini.shanigarapu99@gmail.com>
2024-04-08 19:15:17 +0530

    OpenBrowser

---
+++ src/test/java/com/learn/basicSelenium/LaunchBrowser.java
@@ -0,0 +1,17 @@
+package com.learn.basicSelenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://admin-demo.nopcommerce.com/");
+        Thread.sleep(5);
+        driver.findElement(By.id("Email")).clear();
+        driver.findElement(By.id("Email")).sendKeys("admin@yourstore.com");
+        driver.findElement(By.xpath("//button[contains(text(),'Log in')]")).click();
+    }
+}
